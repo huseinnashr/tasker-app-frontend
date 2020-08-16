01103497e044ec0bc1d28056bd90ef7f4656a2e9
Husein <nvdhunter007@gmail.com>
2020-08-16 19:07:52 +0700

    add login page

--- src/pages/sign-in/sign-in.page.tsx
+++ src/pages/sign-in/sign-in.page.tsx
@@ -1,8 +1,103 @@
+import "./sign-in.page.css";
 import React, { Component } from "react";
-import { Alert } from "antd";
+import { Alert, Row, Col, Typography, Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { inject } from "mobx-react";
+import { AlertMessage } from "../../components";
+import AuthStore from "../../stores/auth.store";
+import { RouterStore } from "mobx-react-router";
+import { AppError } from "../../interfaces";
 
-export class SignInPage extends Component {
+interface SignInPageProps {
+  authStore: AuthStore;
+  routerStore: RouterStore;
+}
+
+interface SignInPageState {
+  signInError: AppError | null;
+  signInLoading: boolean;
+}
+
+@inject("authStore", "routerStore")
+export class SignInPage extends Component<SignInPageProps, SignInPageState> {
+  constructor(props: SignInPageProps) {
+    super(props);
+    this.state = {
+      signInError: null,
+      signInLoading: false,
+    };
+  }
+
+  onFinish = async (data: any) => {
+    const { username, password } = data;
+    this.setState({ signInError: null, signInLoading: true });
+    try {
+      await this.props.authStore.signin(username, password);
+    } catch (e) {
+      this.setState({ signInError: e });
+    }
+    this.setState({ signInLoading: false });
+  };
+
   render() {
-    return <Alert message={"Sign In Page"} type={"info"}></Alert>;
+    return (
+      <Row justify="center" align="middle" style={{ height: "100%" }}>
+        <Col xs={24} md={10} lg={8} xl={6} style={{ padding: "0px 16px" }}>
+          <Row justify="center">
+            <img
+              src="/logo512.png"
+              alt="logo"
+              style={{ height: 46, marginRight: 4 }}
+            ></img>
+            <Typography.Title>Tasker App</Typography.Title>
+          </Row>
+          <Form onFinish={this.onFinish} className="login-form">
+            {this.state.signInError ? (
+              <Alert
+                message={
+                  <AlertMessage message={this.state.signInError.message} />
+                }
+                type="error"
+                style={{ marginBottom: "16px" }}
+                showIcon
+              />
+            ) : null}
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                loading={this.state.signInLoading}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    );
   }
 }
